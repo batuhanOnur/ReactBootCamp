f1a01d74a0141a1e062d86393d2e4fcba4040623
batuhan <batuhanonur29@gmail.com>
2020-02-16 18:36:08 +0300

    jokes app

---
+++ jokeapp/src/JokeList.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './JokeList.css';
+import Joke from './Joke';
+import uuid from 'uuid/v4';
+
+ class JokeList extends Component {
+    static defaultProps = {
+        numJokesToGet: 10
+    };
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    componentDidMount(){
+
+        if(this.state.jokes.length ===0){
+            this.getJokes();
+        }     
+    }
+
+    async getJokes(){
+        let jokes= [];
+        while(jokes.length < this.props.numJokesToGet){
+         
+            //load jokes
+          let res = await axios.get("https://icanhazdadjoke.com/",
+          {headers: {Accept: "application/json"}});
+          jokes.push({id: uuid(),text: res.data.joke, votes: 0});
+        }
+        this.setState(st =>({
+             jokes: [...st.jokes,...jokes]
+        }),
+        ()=> window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+        );
+
+        window.localStorage.setItem(
+            "jokes",
+            JSON.stringify(jokes)
+        );
+        
+    }
+
+    handleVote(id,delta){
+        this.setState(st => ({
+             jokes: st.jokes.map(j =>
+                j.id === id ? {...j,votes: j.votes + delta}: j) 
+        }),
+        ()=> window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+        );
+    }
+
+    handleClick(){
+         this.getJokes();
+    }
+
+    render() {
+        //sort array
+        let jokes = this.state.jokes.sort((a,b) => b.votes -a.votes);
+        return (
+            <div className="JokeList">
+             <div className="JokeList-sidebar">
+                <h1 className="JokeList-title">
+                    <span>Dad</span>Jokes
+                </h1>
+                <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg'/>
+                <button className="JokeList-getmore" onClick={this.handleClick}>Get More</button>
+             </div>
+                
+                <div className="JokeList-jokes">
+                    {jokes.map(j=>(
+                       <Joke 
+                       key={j.id} 
+                       votes={j.votes} 
+                       text={j.text} 
+                       upvote={()=>this.handleVote(j.id,1)}
+                       downvote={()=>this.handleVote(j.id,-1)}
+                       />
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default JokeList;
