7e1b53971c3efb0d71a5da238457b4107e13b17e
batuhan <batuhanonur29@gmail.com>
2020-02-19 03:32:57 +0300

    create custom hook

---
+++ hooks/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import CounterHooks from './CounterHooks';
+import Toggler from './Toggler';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterHooks />
+      <Toggler />
+    </div>
+  );
+}
+
+export default App;
---
+++ hooks/src/Toggler.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import useToggle from './hooks/useToogle';
+
+function Toggler() {
+
+
+    const [isHappy, setIsHappy] = useToggle(true);
+    const [isBroken, setIsBroken] = useToggle(false);
+    
+    
+
+    return (
+        <div>
+           <h1 onClick={setIsHappy}>
+           {isHappy ? "YEEEEEE" : "NOOOOOOOO"}    
+           </h1>
+
+            <h1 onClick={setIsBroken}>
+           {isBroken ? "Yes it is." : "No it is not moron."}    
+           </h1>  
+        </div>
+    )
+}
+
+export default Toggler;
