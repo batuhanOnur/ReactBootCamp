fef3f174106231b5fabdff7284e00362bc34e7c2
batuhan <batuhanonur29@gmail.com>
2020-04-20 14:43:04 +0300

    final

--- mobX/mobx-intro/src/App.js
+++ mobX/mobx-intro/src/App.js
@@ -7,7 +7,14 @@ const StoreContext = React.createContext();
 
 const StoreProvider = ({ children }) => {
   const store = useLocalStore(() => ({
-    bugs: ["Centipede"]
+    bugs: ["Centipede"],
+    addBug: bug => {
+      store.bugs.push(bug);
+    },
+
+    get bugsCount() {
+      return store.bugs.length;
+    }
   }));
 
   return (
@@ -17,22 +24,60 @@ const StoreProvider = ({ children }) => {
   )
 }
 
+const BugsHeader = () => {
+  const store = React.useContext(StoreContext);
+
+  return useObserver(() => (
+    <h1>{store.bugsCount} Bugs !</h1>
+  ))
+}
+
 
 const BugsList = () => {
   const store = React.useContext(StoreContext);
 
-  return (
+  return useObserver(() => (
     <ul>
-      {store.bugs.map(bug => <li key={bug}>{bug}</li>)}
+      {store.bugs.map(bug => (
+        <li key={bug}>{bug}</li>
+      ))}
     </ul>
+  ));
+};
+
+const BugsForm = () => {
+  const store = React.useContext(StoreContext);
+  const [bug, setBug] = React.useState("");
+
+
+  return (
+    <form onSubmit={e => {
+      store.addBug(bug);
+      setBug("");
+      e.preventDefault();
+    }}>
+
+      <input
+        type="text"
+        value={bug}
+        onChange={e => {
+          setBug(e.target.value);
+        }}
+      />
+      <button type="submit">Add</button>
+    </form>
   )
+
 }
+
 
 function App() {
   return (
     <StoreProvider>
       <main>
+        <BugsHeader />
         <BugsList />
+        <BugsForm />
       </main>
     </StoreProvider>
 
